1a38788f84d81643a1e12fe1170fdd8783ab4bd7
Anshima <anshima@Anshimas-MacBook-Pro.local>
2020-06-24 11:42:18 -0400

    Added checks for removing points

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,14 +16,24 @@ export class AppComponent {
   scuba = "../assets/scuba-1.png";
   bolt = "../assets/bolt-1.png";
   skull = "../assets/skull-1.png";
+  bar1 = "../assets/bar.png";
+  bar2 = "../assets/bar.png";
+  bar3="../assets/bar.png";
+  bar4="../assets/bar.png";
+  bar5="../assets/bar.png";
+  bar6="../assets/bar.png";
   first_selected_tree1=false;
   second_selected_tree1=false;
   third_selected_tree1=false;
-// remove_fourth_selected_tree1 = false;
-// remove_third_selected_tree1 = false;
+  remove_second_selected_tree1 = true;
+  remove_third_selected_tree1 = true;
+  remove_fourth_selected_tree1 = true;
   first_selected_tree2 = false;
   second_selected_tree2 = false;
   third_selected_tree2 = false;
+  remove_second_selected_tree2 = true;
+  remove_third_selected_tree2 = true;
+  remove_fourth_selected_tree2 = true;
   id:any;
   event: any;
 
@@ -53,67 +63,90 @@ decrement(event) {
 changeImage(id){  
   if(id==="img1"){
   this.img="../assets/chevron.png";
+  this.bar1 = "../assets/bar-active.png";
   this.first_selected_tree1 = true;
   }
   else if(id==="img2"){
  this.silverware = "../assets/silverware.png";
+ this.bar2 = "../assets/bar-active.png";
  this.second_selected_tree1 = true;
+ this.remove_second_selected_tree1 = false;
   }
 else if(id==="img3"){
 this.cake = "../assets/cake.png";
+this.bar3 = "../assets/bar-active.png";
 this.third_selected_tree1 = true;
+this.remove_third_selected_tree1 = false;
   }
   else if (id==="img4"){
 this.crown = "../assets/crown.png";
+this.remove_fourth_selected_tree1 = false;
+
   }
   else if(id==="img5"){
     this.logo = "../assets/logo.png";
     this.first_selected_tree2 = true;
+    this.bar4 = "../assets/bar-active.png";
   }
   else if(id==="img6"){
     this.scuba = "../assets/scuba.png";
     this.second_selected_tree2 = true;
+    this.remove_second_selected_tree2 = false;
+    this.bar5 = "../assets/bar-active.png";
   }
   else if(id==="img7"){
     this.bolt = "../assets/bolt.png";
     this.third_selected_tree2 = true;
+    this.remove_third_selected_tree2 = false;
+    this.bar6 = "../assets/bar-active.png";
   }
   else if(id==="img8"){
     this.skull = "../assets/skull.png";
+    this.remove_fourth_selected_tree2 = false;
   }
 }
 changeImagedec(id){
   if(id==="img1"){
     this.img="../assets/chevron-1.png";
+    this.bar1 = "../assets/bar.png";
   this.first_selected_tree1 = false;
     }
     else if(id==="img2"){
    this.silverware = "../assets/silverware-1.png";
+   this.bar2 = "../assets/bar.png";
    this.second_selected_tree1 = false;
+   this.remove_second_selected_tree1 = true;
     }
   else if(id==="img3"){
   this.cake = "../assets/cake-1.png";
   this.third_selected_tree1 = false;
-  // this.remove_third_selected_tree1 = true;
+  this.bar3 = "../assets/bar.png";
+  this.remove_third_selected_tree1 = true;
     }
     else if (id==="img4"){
   this.crown = "../assets/crown-1.png";
-  // this.remove_fourth_selected_tree1 = true;
+  this.remove_fourth_selected_tree1 = true;
     }
     else if(id==="img5"){
       this.logo = "../assets/logo-1.png";
       this.first_selected_tree2 = false;
+      this.bar4 = "../assets/bar.png";
     }
     else if(id==="img6"){
       this.scuba = "../assets/scuba-1.png";
       this.second_selected_tree2 = false;
+      this.remove_second_selected_tree2 = true;
+      this.bar5 = "../assets/bar.png";
     }
     else if(id==="img7"){
       this.bolt = "../assets/bolt-1.png";
       this.third_selected_tree2 = false;
+      this.remove_third_selected_tree2 = true;
+      this.bar6 = "../assets/bar.png";
     }
     else if(id==="img8"){
       this.skull = "../assets/skull-1.png";
+        this.remove_fourth_selected_tree2 = true;
     }
   }
 }
